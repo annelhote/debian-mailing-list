4e5d1c81f5c080c0e759de6f415cf62ebbe2ed5a
annelhote <anne.lhote@gmail.com>
2014-12-10 19:49:23 +0100

    Correct the redirection if the url is not relativ. Change start point

--- 01_scrapping/debian.js
+++ 01_scrapping/debian.js
@@ -11,10 +11,13 @@ var sandcrawler 	= require('sandcrawler'),
 
 var iLimit			= 0;
 var sRoottUrl		= "https://lists.debian.org/debian-user/";
-var iYear			= 1996;
-var iMonth			= 1;
 var sStartMessage	= "msg00000.html";
-var sStartUrl		= sRoottUrl + iYear + '/' + (iMonth < 10 ? '0' + iMonth : iMonth) + '/' + sStartMessage;
+// var sStartUrl		= sRoottUrl + iYear + '/' + (iMonth < 10 ? '0' + iMonth : iMonth) + '/' + sStartMessage;
+// var iYear			= 1996;
+// var iMonth			= 1;
+var sStartUrl		= "https://lists.debian.org/debian-user/2001/05/msg04095.html";
+var iYear			= 2001;
+var iMonth			= 5;
 var iEndYear		= 2014;
 var iEndMonth		= 12;
 
@@ -34,7 +37,13 @@ MongoClient.connect('mongodb://127.0.0.1:27017/debian', function(err, db) {
 		.limit(iLimit)
 		.iterate(function(i, req, res) {
 			if(res.data.nextbydate) {
-				sUrl = sRoottUrl + iYear + '/' + (iMonth < 10 ? '0' + iMonth : iMonth) + '/' + res.data.nextbydate;
+				// If the next url is full
+				if(res.data.nextbydate.indexOf('http://') != -1) {
+					sUrl = res.data.nextbydate;
+				// Else build it
+				} else {
+					sUrl = sRoottUrl + iYear + '/' + (iMonth < 10 ? '0' + iMonth : iMonth) + '/' + res.data.nextbydate;	
+				}
 				return sUrl;
 			} else {
 				if((iYear == iEndYear) && (iMonth == iEndMonth)) {
@@ -65,7 +74,6 @@ MongoClient.connect('mongodb://127.0.0.1:27017/debian', function(err, db) {
 			});
 		})
 		.on('job:done', function(job) {
-			/*
 			if(job.state.failing) {
 				MongoClient.connect('mongodb://127.0.0.1:27017/debian', function(err, db) {
 					if(err) throw err;
@@ -78,7 +86,6 @@ MongoClient.connect('mongodb://127.0.0.1:27017/debian', function(err, db) {
 					db.close();
 				});
 			}
-			*/
 		})
 		.result(function(err, req, res) {
 		});
@@ -90,4 +97,3 @@ MongoClient.connect('mongodb://127.0.0.1:27017/debian', function(err, db) {
 		}));
 	});
 });
-
